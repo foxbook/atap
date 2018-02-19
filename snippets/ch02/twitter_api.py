835cc1d69d588250358cd7f650aefee5a5329615
Tony Ojeda <tojeda@districtdatalabs.com>
2018-02-18 19:54:26 -0500

    updating twitter code to match book

--- snippets/ch02/twitter_api.py
+++ snippets/ch02/twitter_api.py
@@ -1,10 +1,9 @@
 import tweepy
-from slugify import slugify
 
-API_KEY = ""
-API_SECRET = ""
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
+API_KEY             = " "
+API_SECRET          = " "
+ACCESS_TOKEN        = " "
+ACCESS_TOKEN_SECRET = " "
 
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -14,10 +13,13 @@ api = tweepy.API(auth)
 users = ["tonyojeda3","bbengfort","RebeccaBilbro","OReillyMedia",
          "datacommunitydc","dataelixir","pythonweekly","KirkDBorne"]
 
-for user in users:
-    user_timeline = api.user_timeline(screen_name=user, count=100)
-    for idx, tweet in enumerate(user_timeline):
-        tweet_text = user_timeline[idx].text
-        filename = slugify(user + " " + tweet_text[:50]) + ".txt"
+def get_tweets(user_list, tweets=20):
+    for user in users:
+        user_timeline = api.user_timeline(screen_name=user, count=tweets)
+        filename = str(user) + ".json"
         with open(filename, 'w+') as f:
-            f.write(tweet_text)
+            for idx, tweet in enumerate(user_timeline):
+                tweet_text = user_timeline[idx].text
+                f.write(tweet_text + "\n")
+
+get_tweets(users, 100)
